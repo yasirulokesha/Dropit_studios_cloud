c0ad01d6ad25807038f785dabb6c96cc7611b603
Yasiru Lokesha <yasirulokesha01@gmail.com>
2021-12-17 15:42:45 +0530

    Commit

--- pages/Components/Background.js
+++ pages/Components/Background.js
@@ -1,29 +1,41 @@
+import { Container } from '@mui/material';
 import {createUseStyles} from 'react-jss'
 
 const styles = createUseStyles({
     Background:{
         zIndex: "-1",
         display:'block',
-        position: "absolute",
-        width: "932.51px",
-        height: "100vh",
-        marginTop: 'auto',
-        marginBottom: 'auto',
+        overflow:"hidden",
+        width:"100vw",
+        maxWidth:"100vw",
+        // position: "absolute",
+        // marginTop: '10vh',
+        // marginBottom: 'auto', 
+        // marginLeft:"25vw",
+        // marginRight:"auto",
+        // height:'fit-content',
+        // width:"fit-content",
+        // // backgroundColor:"#5F5",
+        // width: "600px",
+        // height:"600px",
         filter: "blur(180px)",
-        transform: "rotate(45deg)",
+        // transform: "rotate(50deg)",
+        // transform: "translate(300px,270px)",
+        // overflowX: "hidden",
         // animationName: "bg",
     },
     circle:{
         position: "absolute",
         zIndex:"-3",
         display:"block",
-        position: "absolute",
         width: 490.18,
         height: 468.9,
         background: "#FF5050",
         borderRadius: "272.5px",
         transform: "rotate(-2.43deg)",
-        transform: "translate(550px,-150px)"
+        transform: "translate(170px,0px)",
+        animationName: '$bg',
+        animationTimingFunction: 'ease-in-out',
     },
     circle1:{
         position: "absolute",
@@ -35,7 +47,7 @@ const styles = createUseStyles({
         borderRadius: "100%",
         background: "#49D3FF",
         transform: "rotate(45deg)",
-        transform: "translate(350px,0px)"
+        transform: "translate(0px,70px)"
     },
     circle2:{
         position: "absolute",
@@ -45,21 +57,21 @@ const styles = createUseStyles({
         background: "#7CFFA9",
         borderRadius: 207.5,
         transform: "rotate(45deg)",
-        transform: "translate(650px,150px)"
+        transform: "translate(270px,250px)"
     },
     '@keyframes bg': {
-        from: {transform: "rotate(45deg)"},
-        to: {transform: "rotate(90deg)"}
+        from: {transform: "translate(400px,-150px)"},
+        to: {transform: "translate(550px,-150px)"}
     },
 })
 
 export default function Background(){
     let classes = styles();
     return(
-        <div className={classes.Background}>
+        <Container maxWidth="xl" className={classes.Background}>
             <span className={classes.circle}></span>
             <span className={classes.circle1}></span>
             <span className={classes.circle2}></span>
-        </div>
+        </Container>
     )
 }
