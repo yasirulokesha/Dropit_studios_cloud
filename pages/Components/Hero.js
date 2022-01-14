c57f0466f89d41db7ad0769a658a796f841ef8b0
Yasiru Lokesha <yasirulokesha01@gmail.com>
2022-01-14 12:03:47 +0530

    Arrange Grids & Import Fonts

--- pages/Components/Hero.js
+++ pages/Components/Hero.js
@@ -1,18 +1,37 @@
-import {createUseStyles} from 'react-jss'
+import {createUseStyles} from 'react-jss';
+import {Container, Grid, Typography} from '@mui/material'
 
 const styles = createUseStyles({
     hero:{
-        display:"block",
-        height:"90vh",
-        width: "100vw"
+        display:'flex',
+        // width:"100vw",
+        // maxWidth:"100vw",
+        // position:"absolute",
+        alignItems:"center",
+        flexDirection:"row",
+        height:"100vh",
+        justifyContent:"space-between"
     }
 })
 
 export default function Hero(){
     let classes = styles();
     return(
-        <div className={classes.hero}>
-
-        </div>
+        <Container maxWidth="xl" className={classes.hero}>
+            <Grid container spacing={2}>
+                <Grid item>
+                    <img src='Logo-covered.png'></img>
+                    <Typography fontSize="78pt" fontFamily="medium" lineHeight="70%">
+                        Dropit Studios
+                    </Typography>
+                    <Typography width="70%" fontSize="15pt" fontFamily="light" marginTop="40px">
+                            It’s a greate place to use scripting , filming, Videographic Contents and all of creative things of IT.
+                    </Typography>
+                </Grid>
+            </Grid>
+            <Typography>
+                Hmmm
+            </Typography>
+        </Container>
     )
 }
